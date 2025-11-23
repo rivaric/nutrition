a74246588acdd1d1699cf8b232f8ff8b104d22a0
Rivaric <helium@internet.ru>
2025-11-23 23:30:11 +0300

    feat: enable CORS in main application and update user DTO imports for improved validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
 import { MealModule } from './meal/meal.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule, DatabaseModule, AuthModule, MealModule],
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  });
+
   app.use(cookieParser());
 
   app.useGlobalPipes(
